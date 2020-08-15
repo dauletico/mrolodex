844aa16580df8ee5b8d5603f781ab941363d06d5
Dauletico <dauletico@gmail.com>
2020-08-15 20:09:33 +0300

    Updates

---
+++ precache-manifest.fc8d00b07313286a404ca27f25ac16f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4305bddb80d2d49c4dfd650e11cd8a5b",
+    "url": "/mrolodex/index.html"
+  },
+  {
+    "revision": "30423b3b8447df1a2798",
+    "url": "/mrolodex/static/css/main.22a63ffc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/mrolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mrolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30423b3b8447df1a2798",
+    "url": "/mrolodex/static/js/main.04b68dc2.chunk.js"
+  },
+  {
+    "revision": "8636e5ff1821e2a03160",
+    "url": "/mrolodex/static/js/runtime-main.5c0000b7.js"
+  }
+]);
